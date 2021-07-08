f9c15f04e00c6d6f0d26609a4986b9fda6fd1686
Duncan Skilton <MC03353@mbro.ac.uk>
2021-07-08 14:23:58 +0100

    created new class Enemy

---
+++ app/src/main/java/com/duncancodes/learnjava_android/Enemy.java
@@ -0,0 +1,15 @@
+package com.duncancodes.learnjava_android;
+
+public class Enemy {
+    private String name;
+    private int hitPoints;
+    private int lives;
+
+    public Enemy(String name, int hitPoints, int lives) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.lives = lives;
+    }
+
+    
+}
