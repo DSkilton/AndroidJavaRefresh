79454c604603853b305aeaea79511bdeee5cbfa8
Duncan Skilton <MC03353@mbro.ac.uk>
2021-07-13 08:51:42 +0100

    Added Vampire class

--- app/src/main/java/com/duncancodes/learnjava_android/Main.java
+++ app/src/main/java/com/duncancodes/learnjava_android/Main.java
@@ -3,7 +3,13 @@ package com.duncancodes.learnjava_android;
 public class Main {
     public static void main(String[] args) {
         Troll uglyTroll = new Troll("Ugly Troll");
+        uglyTroll.takeDamage(30);
         uglyTroll.showInfo();
+
+        Vampire vlad = new Vampire ("Vlad");
+        vlad.showInfo();
+        vlad.takeDamage(8);
+        vlad.showInfo();
     }
 
 }
---
+++ app/src/main/java/com/duncancodes/learnjava_android/Vampire.java
@@ -0,0 +1,7 @@
+package com.duncancodes.learnjava_android;
+
+public class Vampire extends Enemy {
+    public Vampire(String name){
+        super(name, 20, 3);
+    }
+}
