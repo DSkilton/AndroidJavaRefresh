b67d01d0d96172206c09186252550d83a98eed11
Duncan Skilton <MC03353@mbro.ac.uk>
2021-07-13 09:16:23 +0100

    using while loop to defeat Drac

--- app/src/main/java/com/duncancodes/learnjava_android/Main.java
+++ app/src/main/java/com/duncancodes/learnjava_android/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         VampireKing dracula = new VampireKing("Dracula");
         dracula.showInfo();
-        dracula.takeDamage(12);
-        dracula.showInfo();
+
+        while(dracula.getLives() > 0){
+            dracula.takeDamage(12);
+            dracula.showInfo();
+        }
     }
 
 }
